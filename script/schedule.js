6e246bac730d11a4ceb266854636cfc0dede2f31
aco <wwsxuan@163.com>
2019-05-30 13:26:01 +0800

    时间格式化错误

--- script/schedule.js
+++ script/schedule.js
@@ -11,7 +11,7 @@ async function main() {
   let data = {
     title: json.title,
     copyright: json.copyright,
-    date: json.enddate,
+    date: json.enddate.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
     location: '',
     filename: filename
   };
